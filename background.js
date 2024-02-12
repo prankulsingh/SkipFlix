f7ed5b2e35f28315c587eeb540b15fa51ae73040
prankulsingh <prankulsinghps@gmail.com>
2024-02-12 20:37:21 +0530

    fixed backdrop issue

--- background.js
+++ background.js
@@ -21,21 +21,6 @@ function displayPopup(skipEnabled) {
             target: {tabId: tab.id},
             func: (skipEnabled) => {
                 const dialog = document.createElement('dialog');
-                dialog.style.cssText = `
-                    position: sticky;
-                    top: 50%;
-                    left: 50%;
-                    transform: translate(-50%, -50%);
-                    background-color: #222;
-                    color: #ccc;
-                    padding: 10px 20px;
-                    border: 1px solid #ccc;
-                    font-size: 40px;
-                    box-shadow: 0 2px 5px rgba(0,0,0,0.5);
-                    border-radius: 10px;
-                    min-width: 160px;
-                    text-align: center;
-                `;
                 dialog.textContent = skipEnabled ? "✅ Skipping ON" : "❌ Skipping OFF"
                 document.body.appendChild(dialog);
                 dialog.showModal();
